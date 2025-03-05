d368f23c4581f313f26331bcb24724015ca77a2b
kess4obehi <kessiena4excellence@gmail.com>
2025-03-05 13:09:08 +0100

    Made  select option interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product) => {
         </div>
 
         <div class="product-quantity-container">
-          <select>
+          <select class="js-quantity-selector-${product.id}">
             <option selected value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -61,6 +61,9 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
 
     let matchingItem;
 
+    const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+    const quantity = Number(quantitySelector.value);
+
     cart.forEach((item) => {
       if (productId === item.productId) {
         matchingItem = item;
@@ -68,11 +71,11 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     });
 
     if (matchingItem) {
-      matchingItem.quantity += 1; 
+      matchingItem.quantity += quantity; 
     } else {
       cart.push({
         productId: productId,
-        quantity: 1
+        quantity: quantity
       });
     }
 
@@ -83,5 +86,7 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     });
 
      document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
+     
   });
 });
